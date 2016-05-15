645c23e7ead57c728081fe533a603246b8b14f03
vipin yadav <vkyadav998@gmail.com>
2016-05-15 14:14:16 +0530

    documentation

--- app4.js
+++ app4.js
@@ -1,6 +1,9 @@
 /**
  * Created by pc on 15-05-2016.
  */
+
+//sent data in body and get response via get and post method
+
 var express =require("express");
 var bodyParser= require("body-parser");
 
@@ -18,10 +21,9 @@ app.get("/getjs",function (req,res) {
 
     var reqjson={"name":req.query.name,
                  "place":req.query.place}
-
     res.json(reqjson);
 });
 
 app.listen(3007,function () {
     console.log("server 7 started");
-})
+});
