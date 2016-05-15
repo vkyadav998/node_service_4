ca765378fc4817f22b8bc793151d6f816d7c64b9
vipin yadav <vkyadav998@gmail.com>
2016-05-15 13:43:41 +0530

    documentation

--- app2.js
+++ app2.js
@@ -1,6 +1,10 @@
 /**
  * Created by pc on 15-05-2016.
  */
+
+//dynamic url params (request params)
+//ex.  http://localhost:3005/vk/vipin/456
+
 var express =require("express");
 var bodyParser= require("body-parser");
 
@@ -9,14 +13,11 @@ var app=new express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 app.get("/vk/:name/:id",function (req,res) {
     var name =req.params.name;
     var id =req.params.id;
     res.send("hellow mr. "+ name +" whats'up?"+" Your id is :"+id);
 });
-
-
 
 app.listen(3005,function (req,res) {
     console.log("server 5 started here");
