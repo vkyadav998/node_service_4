2049adb160fe65da4723612afc4061ad49892de8
vipin yadav <vkyadav998@gmail.com>
2016-05-15 13:56:36 +0530

    documentation

--- app3.js
+++ app3.js
@@ -1,7 +1,10 @@
 /**
  * Created by pc on 15-05-2016.
  */
-//
+
+//dynamic post url params (request params)
+//ex.  http://localhost:3005/vk/vipin/456
+
 var express =require("express");
 var bodyParser= require("body-parser");
 
@@ -10,14 +13,11 @@ var app=new express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 app.post("/vk/:name/:id",function (req,res) {
     var name =req.params.name;
     var id =req.params.id;
     res.send("hellow mr. "+ name +" whats'up?"+" Your id is :"+id);
 });
-
-
 
 app.listen(3006,function (req,res) {
     console.log("server 6 started here");
