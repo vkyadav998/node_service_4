bba8e8d9cbe15b96e8082944b6b62d89fe0acfff
vipin yadav <vkyadav998@gmail.com>
2016-05-15 12:42:11 +0530

    access header

---
+++ app5.js
@@ -0,0 +1,21 @@
+/**
+ * Created by pc on 15-05-2016.
+ */
+
+var express =require("express");
+var bodyParser= require("body-parser");
+
+var app=new express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get("/vk",function (req,res) {
+    var reqjson={"host":req.headers.host}
+    res.json(reqjson);
+});
+
+app.listen(3008,function () {
+    console.log("server 7 started");
+})
