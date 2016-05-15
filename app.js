870d702aab332153ae0472b0bddda3a235ea613f
vipin yadav <vkyadav998@gmail.com>
2016-05-15 10:28:59 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by pc on 15-05-2016.
+ */
+var express =require("express");
+var bodyParser= require("body-parser");
+
+var app=new express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/vk",function (req,res) {
+    var name=req.query.name;
+    var place=req.query.place;
+    res.send("hellow mr. "+ name +" whats'up?"+" R U visited to :"+place);
+});
+
+app.post("/vk",function (req,res) {
+    var name=req.body.name;
+    var place=req.body.place;
+    res.send("hellow mr. "+ name +" whats'up?"+" R U visited to :"+place);
+});
+
+app.listen(3004,function (req,res) {
+    console.log("server 4 is started here");
+})
